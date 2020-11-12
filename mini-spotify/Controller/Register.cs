6df591b8b2eef0871f66a82f269a926c0447a2cd
Alena Ivanova <alenaivanova97poop@gmail.com>
2020-11-12 17:33:38 +0100

    Create Register.cs

---
+++ mini-spotify/Controller/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+namespace mini_spotify.Controller
+{
+    public class Register
+    {
+        private readonly Repository<User> _registerRepository;
+
+        public RegisterController(AppDbContext context)
+        {
+            // create a song repository with the context.
+            _registerRepository = new Repository<User>(context);
+        }
+        /// <summary>
+        /// Adds a user to the database
+        /// </summary>
+        /// <param name="user"></param>
+        public void Add(User user)
+        {
+            if (user.Id == null)
+            {
+                throw new ArgumentNullException("id");
+            }
+
+            //TO DO: check on values of required properties. 
+            // For more information check the Acceptation Criteria.
+
+            _registerRepository.Add(user);
+            _registerRepository.SaveChanges();
+        }
+    }
+}
