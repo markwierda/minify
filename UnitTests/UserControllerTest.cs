dd5fd651342dfdc53792554aa1111d74caf9303b
Mark Wierda <markwierda14@gmail.com>
2020-11-15 18:18:44 +0100

    Revert "22/validation"

---
+++ UnitTests/UserControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using mini_spotify.Controller;
+using mini_spotify.DAL;
+using mini_spotify.DAL.Entities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public class UserControllerTest
+    {
+        private readonly string testPassword = "Test!123";
+        private readonly string testPasswordHashed = "p+eUD89OFO/VOk+Ca1Qq+0w1pyp8A6maF/u/gQrH+Icp3GQp";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void HashPassword_Return_NotNull()
+        {
+           string hash = UserController.HashPassword(testPassword);
+
+            Assert.NotNull(hash);
+        }
+
+        [Test]
+        public void ValidatePassword_Return_True()
+        {
+            bool hash = UserController.ValidatePassword(testPassword, testPasswordHashed);
+
+            Assert.IsTrue(hash);
+        }
+
+        [Test]
+        public void ValidatePassword_Return_False()
+        {
+            bool hash = UserController.ValidatePassword("AnderWachtwoord:)" , testPasswordHashed);
+
+            Assert.IsFalse(hash);
+        }
+    }
+}
