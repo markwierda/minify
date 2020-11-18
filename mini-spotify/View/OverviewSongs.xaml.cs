0048b6c2858f0f9fd9ada15ec98f0c9cf829b492
alenaivanova97 <s1146589@student.windesheim.nl>
2020-11-18 14:24:47 +0100

    add overview song

---
+++ mini-spotify/View/OverviewSongs.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mini_spotify.View
+{
+    /// <summary>
+    /// Interaction logic for OverviewSongs.xaml
+    /// </summary>
+    public partial class OverviewSongs : Window
+    {
+        public OverviewSongs()
+        {
+            InitializeComponent();
+            this.Title = "Hello world";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Title = e.GetPosition(this).ToString();
+
+        }
+    }
+}
