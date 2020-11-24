0ae626b88b82b35b59ccd6ba34a2bedd903a2bfa
SilentSwaps <leroyveldhoen@gmail.com>
2020-11-24 15:00:54 +0100

    left hitlist overview, will scroll with more hitlists

--- mini-spotify/View/Overview.xaml.cs
+++ mini-spotify/View/Overview.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using mini_spotify.DAL.Entities;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace mini_spotify.View
 {
@@ -10,6 +12,46 @@ namespace mini_spotify.View
         public Overview()
         {
             InitializeComponent();
+        }
+
+        private void Initialize(object sender, RoutedEventArgs e)
+        {
+            GetAllHitList();
+        }
+
+        public void GetAllHitList()
+        {
+            List<Song> items = new List<Song>
+            {
+                new Song() { Name = "John Doe", Genre = "Huh", Duration = 1, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
+                new Song() { Name = "John", Genre = "Naar huis", Duration = 56, Path = "/path/to/song" }
+            };
+
+            HitlistMenu.ItemsSource = items;
         }
     }
 }
