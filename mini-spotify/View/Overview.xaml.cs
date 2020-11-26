19b015f84356d2457eab51f60eaf5dd85c46e94b
alenaivanova97 <s1146589@student.windesheim.nl>
2020-11-26 15:12:48 +0100

    koppeling hitlist

--- mini-spotify/View/Overview.xaml.cs
+++ mini-spotify/View/Overview.xaml.cs
@@ -1,4 +1,7 @@
-﻿using mini_spotify.DAL.Entities;
+﻿using mini_spotify.Controller;
+using mini_spotify.DAL.Entities;
+using mini_spotify.Model;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 
@@ -9,9 +12,12 @@ namespace mini_spotify.View
     /// </summary>
     public partial class Overview : Window
     {
+
+        private HitlistController _hitlistController;
         public Overview()
         {
             InitializeComponent();
+            _hitlistController =new HitlistController();
         }
 
         private void Initialize(object sender, RoutedEventArgs e)
@@ -22,7 +28,7 @@ namespace mini_spotify.View
         public void GetAllHitList()
         {
             //TODO: Add a check if given list is empty
-            List<Song> items = new List<Song>
+            /*List<Song> items = new List<Song>
             {
                 new Song() { Name = "John Doe", Genre = "Huh", Duration = 1, Path = "/path/to/song" },
                 new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
@@ -50,9 +56,11 @@ namespace mini_spotify.View
                 new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
                 new Song() { Name = "Huh", Genre = "Piet", Duration = 4, Path = "/path/to/song" },
                 new Song() { Name = "John", Genre = "Naar huis", Duration = 56, Path = "/path/to/song" }
-            };
+            };*/
 
-            HitlistMenu.ItemsSource = items;
+            List<Hitlist> hitlists = _hitlistController.GetHitlistsByUserId(AppData.Id);
+
+            HitlistMenu.ItemsSource = hitlists;
 
         }
 
