6720f139cc4b6b9329fa97545cffc5438771dead
SilentSwaps <leroyveldhoen@gmail.com>
2020-11-27 13:34:40 +0100

    mediaplayer buttons

--- mini-spotify/View/Overview.xaml.cs
+++ mini-spotify/View/Overview.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using mini_spotify.Controller;
 using mini_spotify.DAL.Entities;
 using mini_spotify.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
 
 namespace mini_spotify.View
@@ -15,19 +18,19 @@ namespace mini_spotify.View
 
         public Overview()
         {
-            InitializeComponent();
-            _hitlistController =new HitlistController();
+            //InitializeComponent();
+            //_hitlistController =new HitlistController();
         }
 
         private void Initialize(object sender, RoutedEventArgs e)
         {
-            GetAllHitList();
+            //GetAllHitList();
         }
 
         public void GetAllHitList()
         {
-            List<Hitlist> hitlists = _hitlistController.GetHitlistsByUserId(AppData.Id);
-            HitlistMenu.ItemsSource = hitlists;
+            //List<Hitlist> hitlists = _hitlistController.GetHitlistsByUserId(AppData.Id);
+            //HitlistMenu.ItemsSource = hitlists;
         }
 
         private void btn_Add_Hitlist(object sender, RoutedEventArgs e)
@@ -37,10 +40,36 @@ namespace mini_spotify.View
 
         private void HitlistMenu_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            Hitlist selected = (Hitlist)e.AddedItems[0];
-            OverviewHitlist overviewHitlist = new OverviewHitlist(selected.Id);
-            overviewHitlist.Show();
-            Close();
+            //Hitlist selected = (Hitlist)e.AddedItems[0];
+            //OverviewHitlist overviewHitlist = new OverviewHitlist(selected.Id);
+            //overviewHitlist.Show();
+            //Close();
+        }
+
+        //mediaplayer handlers
+
+        public void OnMouseDownPlay(object sender, RoutedEventArgs e)
+        {
+            //change play button into pause button
+            btn_Play.Visibility = Visibility.Collapsed;
+            btn_Pause.Visibility = Visibility.Visible;
+        }
+        
+        public void OnMouseDownPause(object sender, RoutedEventArgs e)
+        {
+            //change pause button into play button
+            btn_Pause.Visibility = Visibility.Collapsed;
+            btn_Play.Visibility = Visibility.Visible;
+        }
+
+        private void OnMouseDownBack(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void OnMouseDownNext(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
