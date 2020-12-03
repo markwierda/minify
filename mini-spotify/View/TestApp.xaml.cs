9b88baad91e731a99f4214edd7986343440f8316
Mark Wierda <markwierda14@gmail.com>
2020-12-03 15:10:38 +0100

    Test dialog

---
+++ mini-spotify/View/TestApp.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mini_spotify.View
+{
+    /// <summary>
+    /// Interaction logic for TestApp.xaml
+    /// </summary>
+    public partial class TestApp : Window
+    {
+        public TestApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnterName_Click(object sender, RoutedEventArgs e)
+        {
+            TestDialog inputDialog = new TestDialog("Please enter your name:", "John Doe");
+            if (inputDialog.ShowDialog() == true)
+                lblName.Text = inputDialog.Answer;
+        }
+    }
+}
